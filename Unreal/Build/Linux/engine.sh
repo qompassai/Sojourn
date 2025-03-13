d37abd21586070c35fdc591be97390891057fafd
PhaedrusFlow <map@qompass.ai>
2025-03-12 21:45:59 -0700

    update

---
+++ Unreal/Build/Linux/engine.sh
@@ -0,0 +1,2 @@
+./Engine/Build/BatchFiles/RunUAT.sh BuildGraph -script=Engine/Build/InstalledEngineBuild.xml -target="Make Installed Build Linux" -set:SignExecutables=true -set:GameConfigurations=Debug;Development;Test;Shipping -set:WithLinux=true -set:WithLinuxArm64=true -set:WithDDC=true -set:CompileDatasmithPlugins=true -set:CompileUSDImporter=true -set:ExtraCompileArgs="-bCompileCEF3 -bCompileISPC" -set:VulkanRayTracing=true -set:VulkanTargetedShaderFormats=SF_VULKAN_SM6
+
